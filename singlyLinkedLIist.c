2a3aa82782a4aed256744cd7220cdc2450106e68
amaan wanie <amaanwanie5@gmail.com>
2023-07-28 09:56:02 +0530

    added some comments

--- singlyLinkedLIist.c
+++ singlyLinkedLIist.c
@@ -1,101 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct sll {
-	int D;
-	struct sll *l;
+    int D;
+    struct sll* l;
 };
-struct sll * start=NULL;
-struct sll * end=NULL;
 
-void insertFront();
-void insertEnd();
+struct sll* start = NULL;
+struct sll* end = NULL;
 
-void delFront();
-void delEnd();
-void display();
+// Function prototypes
+void insertFront(); // Insert element at the front of the linked list
+void insertEnd();   // Insert element at the end of the linked list
+void delFront();    // Delete element from the front of the linked list
+void delEnd();      // Delete element from the end of the linked list
+void display();     // Display the entire linked list
 
-int main(){
-	int a;
-	do{
-		printf("press 1 for insertAtFront\n");
-        printf("press 2 for insertAtEnd\n");
-        printf("press 3 for deleteAtFront\n");
-        printf("press 4 for deleteAtEnd\n");
-        printf("press 5 for display\n");
-        scanf("%d",&a);
-        
-        switch(a){
-         	case 1 :
-        		insertFront();
-        		break;
-        	case 2 :
+int main() {
+    int a;
+    do {
+        printf("Press 1 for insertAtFront\n");
+        printf("Press 2 for insertAtEnd\n");
+        printf("Press 3 for deleteAtFront\n");
+        printf("Press 4 for deleteAtEnd\n");
+        printf("Press 5 for display\n");
+        printf("Press 0 to exit\n");
+        scanf("%d", &a);
+
+        switch (a) {
+        case 1:
+            insertFront();
+            break;
+        case 2:
             insertEnd();
-        	    break;
-         	case 3 :
-     		delFront();
-        		break;
-        	case 4 :
-        		delEnd();
-        		break;
-        	case 5 :
-       		display();
-        		break;
-        	default :
-        		break;
-		}
-	}
-	while (a!=0);
-	return 0;
-	
-}
-void insertFront(){
-	struct sll *p;
-	int el;
-	p=(struct sll *)malloc(sizeof(struct sll));
-	printf("enter the element");
-	scanf("%d",&el);
-		p->D=el;
-		p->l=start;
-	if(start==NULL){
-	
-		start=p;
-		end=p;
-	//	free(p);
-		
-	}
-	else {
-		start=p;
-		
-	}
-}
-void insertEnd(){
-	struct sll *p;
-	if(start==NULL){
-		printf("empty sll");
-		return;
-	}
-	int el;
-	p=(struct sll *)malloc(sizeof(struct sll));
-	
-	printf("enter the element");
-	scanf("%d",&el);
-	p->D=el;
-	p->l=NULL;
-	if(start==NULL){
-	
-		start=p;
-		end=p;
-	//	free(p);
-		
-	}
-	else{
-		end->l=p;
-		end = p;
-		
-	}
-	
+            break;
+        case 3:
+            delFront();
+            break;
+        case 4:
+            delEnd();
+            break;
+        case 5:
+            display();
+            break;
+        default:
+            break;
+        }
+    } while (a != 0);
+    return 0;
 }
 
+void insertFront() {
+    struct sll* p;
+    int el;
+    p = (struct sll*)malloc(sizeof(struct sll));
+    printf("Enter the element: ");
+    scanf("%d", &el);
+    p->D = el;
+    p->l = start;
+    start = p;
+}
+
+void insertEnd() {
+    struct sll* p;
+    int el;
+    p = (struct sll*)malloc(sizeof(struct sll));
+    printf("Enter the element: ");
+    scanf("%d", &el);
+    p->D = el;
+    p->l = NULL;
+    if (start == NULL) {
+        // If the list is empty, set both start and end to the new node
+        start = p;
+        end = p;
+    } else {
+        // Otherwise, add the new node to the end and update the end pointer
+        end->l = p;
+        end = p;
+    }
+}
 
 void display() {
     struct sll* p;
@@ -104,47 +87,48 @@ void display() {
         return;
     }
     p = start;
-    
+    printf("Linked List: ");
     while (p != NULL) {
-       // printf("%d ", p->D);
-       printf("el=: %d link =:%d pointer=:%d\n",p->D,p->l,p);
+        printf("%d ", p->D);
         p = p->l;
     }
     printf("\n");
-    printf("start=%d\n",start);
-    printf("end=%d\n",end);
+    printf("start=%p\n", (void*)start);
+    printf("end=%p\n", (void*)end);
 }
-void delFront(){
-	struct sll *p;
-	if(start==NULL){
-		printf("stack empty");
-		return;
-	}	
-		p=start;
-		p=p->l;
-		start=p;
-	
-	
+
+void delFront() {
+    struct sll* p;
+    if (start == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    p = start;
+    start = p->l;
+    free(p);
 }
-void delEnd(){
-	struct sll *p;
-	if(start==NULL){
-		printf("stack empty");
-		return;
-	}
-	p=start;
-	if (start->l == NULL) {
+
+void delEnd() {
+    struct sll* p;
+    if (start == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    if (start->l == NULL) {
+        // If there is only one element, delete it and set both start and end to NULL
+        printf("The element deleted is %d\n", start->D);
         free(start);
         start = NULL;
         end = NULL;
         return;
-   }
-      
-	
-	while(p->l->l!=NULL){
-		p=p->l;
-	}
-	printf("the ele deleted is %d",p->l->D);
-	p->l=NULL;
-	end = p;
+    }
+    p = start;
+    while (p->l->l != NULL) {
+        // Traverse to the second last element
+        p = p->l;
+    }
+    printf("The element deleted is %d\n", p->l->D);
+    free(p->l);
+    p->l = NULL;
+    end = p;
 }
