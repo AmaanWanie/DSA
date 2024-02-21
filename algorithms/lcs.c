09bf29ceab415772c2e2c27af969a92335cf1310
unknown <amaanwanie5@gmail.com>
2024-02-21 12:44:12 +0530

    fixed lcs

--- algorithms/lcs.c
+++ algorithms/lcs.c
@@ -1,112 +1,64 @@
 #include<stdio.h>
 
-/*
+void LCS(char X[], char Y[], int len_X, int len_Y);
+int maxValue(int a, int b);
+void printCostMatrix(int cost[6][6], int len_X, int len_Y);
 
-   if(x[i]==y[j]){
-       
-	   cost[i][j]=1+cost[i-1][j-1] 
-  }
-  else{
-     
-  cost[i][j]=   maxValue(cost[i][j-1],cost[i-1][j])
-  
-}
-
-*/
-
-/*
-
-
-algorithm LCS(X[],Y[],cost[][])
-
-input:
-output:
-desc:
-
-variable integer := i,j;
-
-for (i=0;i<len_X) do//step=step+1
-	cost[0][j]=0
-
-	end for
-
-for (j=0;i<len_Y) do //step=step+1
-	cost[i][0]=0
-	
-	end for
-
-for (i=1 < len_X)do //step=step+1
-	
-	for(j=1 < len_Y)//step=step+1
-	
-		if (X[i]==Y[j])
-		   cost[i][j]= 1 + cost[i-1][j-1]
-		
-		else
-		cost[i][j]= maxValue(cost[i-1][j],cost[i][j-1])
-		
-	end for
-
-end for	
-
-*/
-int maxValue(int a,int b){
-	
-	if(a>=b){
-		return a;
-	}else{
-		return b;
-	}
-	
-	
-	
-	
-	
-}
-void LCS(char X[5],char Y[5],int cost[5][5]){
-
-	int i=0,j=0;
-	
-	for(i=0;i<5;i++){
-		cost[i][0]=0;
-	}
-	
-	for(j=0;j<5;j++){
-		cost[0][j]=0;
-	}
-	
-	for(i=1;i<=4;i++){
-		//i side top to down
-		for(j=1;j<=4;j++){
-			//j side left to right
-			
-		if (X[i]==Y[j]){
-		
-		   cost[i][j]= 1 + cost[i-1][j-1];
-	}
-		else{
-		
-		int a =	cost[i-1][j];
-		int b =	cost[i][j-1];
-		
-		cost[i][j]= maxValue(a,b);
-			}
-		}
-		
-		
-		
-	}
-	printf("the longest seq is: %d ",cost[5][5]);
+int maxValue(int a, int b) {
+    if (a >= b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
 
-int main(){
-	
-	char X[5] = {'a', 'b', 'a', 'a', 'b'};
-	char Y[5] = {'b', 'a', 'b', 'b', 'a'};
+void printCostMatrix(int cost[6][6], int len_X, int len_Y) {
+    printf("Cost Matrix:\n");
+    for (int i = 0; i <= len_X; i++) {
+        for (int j = 0; j <= len_Y; j++) {
+            printf("%d\t", cost[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-	int cost[5][5];
-	
-	LCS(X,Y,cost);
-	
+void LCS(char X[], char Y[], int len_X, int len_Y) {
+    int cost[len_X + 1][len_Y + 1];
+    
+    for(int i=0;i<=len_X;i++){
+    	cost[i][0]=0;
+	}
+	for(int j=0;j<=len_Y;j++){
+    	cost[0][j]=0;
+	}
+    
+
+    for (int i = 1; i <= len_X; i++) {
+        for (int j = 1; j <= len_Y; j++) {
+        	
+        	
+             if (X[i - 1] == Y[j - 1])
+                cost[i][j] = 1 + cost[i - 1][j - 1];
+            else
+                cost[i][j] = maxValue(cost[i - 1][j], cost[i][j - 1]);
+        }
+    }
+
+
+    printf("The length of the longest subsequence is: %d\n", cost[len_X][len_Y]);
+    
+    printCostMatrix(cost, len_X, len_Y);
+}
+
+int main() {
+    char X[] = {'a', 'b', 'a', 'a', 'b'};
+    char Y[] = {'b', 'a', 'b', 'b', 'a'};
+
+    int len_X = sizeof(X) / sizeof(X[0]);
+    int len_Y = sizeof(Y) / sizeof(Y[0]);
+
+    LCS(X, Y, len_X, len_Y);
+
+    return 0;
 }
